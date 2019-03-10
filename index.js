d6599906e21a94a2de8bae9c597ce091b113a5eb
Valentin Berlier <berlier.v@gmail.com>
2019-03-10 17:34:49 -0400

    Add document field

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const path = require('path')
+
+const docutils = require('docutils-parser')
+const defaultsDeep = require('lodash/defaultsDeep')
+
 class DocutilsTransformer {
   static mimeTypes () {
     return ['application/xml']
+  }
+
+  constructor (options, { context, localOptions }) {
+    const { basePath } = defaultsDeep(localOptions, options, {
+      basePath: context
+    })
+
+    this.basePath = path.normalize(basePath)
   }
 
   parse (content) {
@@ -9,8 +22,13 @@ class DocutilsTransformer {
     }
   }
 
-  extendNodeType () {
-    return {}
+  extendNodeType ({ graphql: { GraphQLJSON } }) {
+    return {
+      document: {
+        type: GraphQLJSON,
+        resolve: node => docutils.parseDocument(node.content)
+      }
+    }
   }
 }
 

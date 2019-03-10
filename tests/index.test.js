bb982c650e188e1104c5ab7d60f434634e1a8c0a
Valentin Berlier <berlier.v@gmail.com>
2019-03-10 18:23:18 -0400

    Setup jest

---
+++ tests/index.test.js
@@ -0,0 +1,24 @@
+const DocutilsTransformer = require('../index')
+
+const basicDoc = require('./documents/hello.xml')
+
+describe('parse method', () => {
+  let transformer
+
+  beforeEach(() => {
+    transformer = new DocutilsTransformer(
+      {
+        basePath: 'documents/'
+      },
+      {
+        context: __dirname,
+        localOptions: {}
+      }
+    )
+  })
+
+  test('returns the content as-is', () => {
+    const { content } = transformer.parse(basicDoc)
+    expect(content).toEqual(basicDoc)
+  })
+})

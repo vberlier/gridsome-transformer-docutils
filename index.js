376401df2d16996d471b8d15d755179aa95e25b2
Valentin Berlier <berlier.v@gmail.com>
2019-03-11 23:06:59 -0400

    Update dependencies

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class DocutilsTransformer {
     return {
       document: {
         type: GraphQLJSON,
-        resolve: node => docutils.parseDocument(node.content)
+        resolve: node => docutils.parse(node.content)
       }
     }
   }

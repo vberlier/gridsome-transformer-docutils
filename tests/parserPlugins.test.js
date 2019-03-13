469d46658c7778d78d8a673c5ee5c3576f6632b4
Valentin Berlier <berlier.v@gmail.com>
2019-03-13 15:22:41 -0400

    Test extractSourcePath plugin

--- tests/parserPlugins.test.js
+++ tests/parserPlugins.test.js
@@ -1,6 +1,6 @@
 const docutils = require('docutils-parser')
 
-const { extractDocumentTitle } = require('../lib/parserPlugins')
+const { extractDocumentTitle, extractSourcePath } = require('../lib/parserPlugins')
 
 const basicDoc = require('./documents/hello.xml')
 const testDoc = require('./documents/test.xml')
@@ -20,5 +20,22 @@ describe('document title', () => {
     docutils.parse(testDoc, [extractDocumentTitle(documentTitle => { title = documentTitle })])
 
     expect(title).toEqual('This is a test')
+  })
+})
+describe('source path', () => {
+  test('default basePath', () => {
+    let source = ''
+
+    docutils.parse(basicDoc, [extractSourcePath('/', sourcePath => { source = sourcePath })])
+
+    expect(source).toEqual('hello')
+  })
+
+  test('custom basePath', () => {
+    let source = ''
+
+    docutils.parse(basicDoc, [extractSourcePath('example-project/', sourcePath => { source = sourcePath })])
+
+    expect(source).toEqual('docs/hello')
   })
 })

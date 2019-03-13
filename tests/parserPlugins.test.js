2d48cadd1793cb7343a35f6c6bc964c535bf893d
Valentin Berlier <berlier.v@gmail.com>
2019-03-13 15:14:31 -0400

    Test extractDocumentTitle plugin

---
+++ tests/parserPlugins.test.js
@@ -0,0 +1,24 @@
+const docutils = require('docutils-parser')
+
+const { extractDocumentTitle } = require('../lib/parserPlugins')
+
+const basicDoc = require('./documents/hello.xml')
+const testDoc = require('./documents/test.xml')
+
+describe('document title', () => {
+  test('simple', () => {
+    let title = ''
+
+    docutils.parse(basicDoc, [extractDocumentTitle(docutmentTitle => { title = docutmentTitle })])
+
+    expect(title).toEqual('Hello, world!')
+  })
+
+  test('literal element', () => {
+    let title = ''
+
+    docutils.parse(testDoc, [extractDocumentTitle(documentTitle => { title = documentTitle })])
+
+    expect(title).toEqual('This is a test')
+  })
+})
